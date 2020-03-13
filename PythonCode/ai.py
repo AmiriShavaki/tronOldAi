94a37bb40c39ee5d1dba1a4306f347277d86066b
Mahdi Amiri Shavaki <mahdi_amiri98@comp.iust.ac.ir>
2020-03-13 12:38:34 +0330

    better ai replaced

--- PythonCode/ai.py
+++ PythonCode/ai.py
@@ -43,12 +43,18 @@ class AI(RealtimeAI):
     def isValidPos(self, x, y):
         return self.m > x >= 0 and self.n > y >= 0
 
+    def getOppPos(self):
+        return [self.world.agents[self.other_side].position.x, self.world.agents[self.other_side].position.y]
+
+    def isEmpty(self, x, y):
+        return self.world.board[y][x] == ECell.Empty and self.getOppPos() != [x, y]
+    
     def numberOfEmptyNeighbors(self, x, y):
         ans = 0
         for i in range(4):
             newX = x + self.xDir[i]
             newY = y + self.yDir[i]
-            if self.isValidPos(newX, newY) and self.world.board[newY][newX] == ECell.Empty:
+            if self.isValidPos(newX, newY) and self.isEmpty(newX, newY):
                 ans += 1
         return ans
 
@@ -59,7 +65,29 @@ class AI(RealtimeAI):
         for i in range(4):
             newX = x + self.xDir[i]
             newY = y + self.yDir[i]
-            if self.isValidPos(newX, newY) and self.world.board[newY][newX] == ECell.Empty:
+            if self.isValidPos(newX, newY) and self.isEmpty(newX, newY):
+                ans.append(i)
+        return ans
+
+    def playerWallNeighbors(self, x, y):
+        ans = []
+        xDir = [0, 1, 0, -1]
+        yDir = [-1, 0, 1, 0]
+        for i in range(4):
+            newX = x + self.xDir[i]
+            newY = y + self.yDir[i]
+            if self.isValidPos(newX, newY) and self.world.board[newY][newX] in [ECell.BlueWall, ECell.YellowWall]:
+                ans.append(i)
+        return ans
+
+    def notAreaWallNeighbors(self, x, y):
+        ans = []
+        xDir = [0, 1, 0, -1]
+        yDir = [-1, 0, 1, 0]
+        for i in range(4):
+            newX = x + self.xDir[i]
+            newY = y + self.yDir[i]
+            if self.isValidPos(newX, newY) and self.world.board[newY][newX] != ECell.AreaWall:
                 ans.append(i)
         return ans
                 
@@ -94,6 +122,27 @@ class AI(RealtimeAI):
         print(ans)
         return ans
 
+    def reachableEmptyWB(self, x, y, curDir):
+        originX = x
+        originY = y
+        x += self.xDir[curDir]
+        y += self.yDir[curDir]
+        queue = deque([[x, y, 1]])
+        mark = [[-1] * self.m for i in range(self.n)]
+        mark[y][x] = 0
+        while len(queue) > 0:
+            left = queue.popleft()            
+            for i in self.notAreaWallNeighbors(left[0], left[1]):
+                newX = left[0] + self.xDir[i]
+                newY = left[1] + self.yDir[i]
+                if self.isEmpty(newX, newY) and (newX != originX or newY != originY):
+                    return left[2] + 1
+                if mark[newY][newX] == -1 and (newX != originX or newY != originY):
+                    queue.append([newX, newY, left[2] + 1])
+                    mark[newY][newX] = mark[left[1]][left[0]] + 1
+        print("Not found")
+        return float("inf")
+
     def mostOpenDecision(self, x, y, moves):
         ma = -1
         ans = -1
@@ -107,7 +156,7 @@ class AI(RealtimeAI):
         return ans
     
     def decide(self):
-        print('decide', self.current_cycle)
+        print('decide', self.current_cycle, self.my_side)
         #every cycle
         #500ms
         curX = self.world.agents[self.my_side].position.x
@@ -124,7 +173,24 @@ class AI(RealtimeAI):
                 betterMoves = [i]
             elif res == ma:
                 betterMoves.append(i)
-        
-        decision = self.mostOpenDecision(curX, curY, betterMoves)
-        
+                
+        if len(betterMoves) > 0:
+            print("betterMoves: ", betterMoves)
+            decision = self.mostOpenDecision(curX, curY, betterMoves)
+        else:
+            moves = self.playerWallNeighbors(curX, curY)
+            res = [float("inf")] * 4
+            mi = float("inf")
+            ans = 0
+            for i in moves:
+                res[i] = self.reachableEmptyWB(curX, curY, i)
+                if res[i] < mi:
+                    mi = res[i]
+                    ans = i
+            print("res: ", res)
+            print(self.convertIndToDir(ans), "WB")
+            decision = ans
+            if self.world.agents[self.my_side].wall_breaker_cooldown == 0:
+                self.send_command(ActivateWallBreaker())
+                
         self.send_command(ChangeDirection(self.convertIndToDir(decision)))
